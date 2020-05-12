5b7eda88a353f76321eb82190b649d76ad1739db
ankit <ankitkejriwaal@gmail.com>
2020-05-12 13:44:11 -0400

    added user model and validation for users

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+const { check, validationResult } = require('express-validator/check');
+
+const User = require('../models/User');
+
 // @route   POST  api/users
 // @desc Register a user
 // @access Public
-router.post('/', (req, res) => {
-    res.send('Register a user');
+router.post('/',[
+    check('name','Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password','Minimum length required is 6').isLength({min:6})
+], (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+    
+    res.send(req.body);
 });
 
 module.exports = router;
