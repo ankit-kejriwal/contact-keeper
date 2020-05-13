929cd4e81d796342b89b8a4f6b8fccd245e79099
ankit <ankitkejriwaal@gmail.com>
2020-05-13 19:10:00 -0400

    setup the client side for the application

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1>About the app</h1>
+            <div className="my-1">
+                <strong>Full stack react app for contacts</strong>
+            </div>
+            <div className="bg-dark p">
+                <strong>Version: 1.0.0</strong>
+            </div>
+            
+        </div>
+    )
+}
+
+export default About;
