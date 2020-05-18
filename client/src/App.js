e3811b22b4cad3f37541fe2ed60a623a19367e59
ankit <ankitkejriwaal@gmail.com>
2020-05-18 15:53:17 -0400

    added resiter and authentication part

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import About from './components/pages/About';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
+import Alerts from './components/layout/Alerts';
+import setAuthToken from './utils/setAuthToken';
 
 import Navbar from './components/layout/Navbar';
 import ContactState from './context/contact/ContactState';
@@ -15,6 +17,9 @@ import AlertState from './context/alert/AlertState';
 import './App.css';
 
 const App = () => {
+  if(localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
   return (
     <AuthState>
       <ContactState>
@@ -23,12 +28,13 @@ const App = () => {
             <Fragment>
               <Navbar />
               <div className="container">
-                <Switch>
-                  <Route exact path ='/' component={Home} />
-                  <Route exact path ='/about' component={About} />
-                  <Route exact path ='/register' component={Register} />
-                  <Route exact path ='/login' component={Login} />
-                </Switch>
+                <Alerts />
+                  <Switch>
+                    <Route exact path ='/' component={Home} />
+                    <Route exact path ='/about' component={About} />
+                    <Route exact path ='/register' component={Register} />
+                    <Route exact path ='/login' component={Login} />
+                  </Switch>
               </div>
             </Fragment>
           </Router>
