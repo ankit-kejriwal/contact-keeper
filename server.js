76187982a5b0d86bcb3d50c29475b1348f9bb152
ankit <ankitkejriwaal@gmail.com>
2020-05-18 18:09:07 -0400

    updated json for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 const app = express();
 
 // connect database
@@ -17,6 +18,13 @@ app.get('/', (req, res) =>
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
+
+// serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+    // Set Static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 
 const PORT = process.env.PORT || 5000;
 
